fcacacad2cf4e48d0afcef2b93168e3528087dbb
Andrew Proctor <procta06@gmail.com>
2015-03-25 00:36:29 -0700

    Added ipod neccesities. I want these spotify songs!

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -25,3 +25,22 @@ $ git config --global user.name "Andrew Proctor"
 $ git config --global user.email procta06@gmail.com
 $ git config --global core.editor vim
 $ git config --global merge.ff false
+
+#Spotify
+# 1. Add this line to your list of repositories by
+#    editing your /etc/apt/sources.list
+deb http://repository.spotify.com stable non-free
+
+# 2. If you want to verify the downloaded packages,
+#    you will need to add our public key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
+
+# 3. Run apt-get update
+sudo apt-get update
+
+# 4. Install spotify!
+sudo apt-get install spotify-client
+
+#Ipod Mounting
+sudo apt-get install ideviceinstaller
+ideviceinstaller -install
